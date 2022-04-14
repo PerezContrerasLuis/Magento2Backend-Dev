b978e7c7206cb763478a629028a26d903542992b
PerezContrerasLuis <luisperezcontreras@gmail.com>
2022-04-14 13:50:02 -0500

    Creación de Modulo en Magento 2. url (local) para este proyecto: magento2.local
    0.- Creamos la estructura para nuestro nuevo módulo.
    1.- creamos nuestro archivo para definir la información del módulo:  app\code\LuisDev\FirstModule\etc\module.xml
    2.- creamos nuestro archivo para registrar el modulo en la BDD:  app\code\LuisDev\FirstModule\registration.php
    3.- corremos comando para actualizar base de datos : php bin/magento setup:upgrade
    Impoartante verificar que en : app\etc\config.php este nuestro modulo 'LuisDev_FirstModule' => 1,

---
+++ code/LuisDev/FirstModule/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'LuisDev_FirstModule', __DIR__);
